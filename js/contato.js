ee05e279605bc0d49fbff72c43a77c5c04a88e39
EllenOli <ellenplush@gmail.com>
2021-05-16 12:21:11 -0300

    Alterando páginas de contato e index e linkando para acessar as demais.

---
+++ js/contato.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector ('.navbar-toggler');
+const navbar = document.querySelector ('.navbar');
+
+btn.addEventListener('click', e => {
+
+  navbar.classList.toggle('sidebar-open');
+
+})
--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,9 @@
 
-//Adicionando sidebar e evento de click
-const btn = document.querySelector(".navbar-toggler");
-const navbar = document.querySelector(".navbar");
-btn.addEventListener("click", e => {
-    navbar.classList.toggle("sidebar-open")
+const btn = document.querySelector ('.navbar-toggler');
+const navbar = document.querySelector ('.navbar');
+
+btn.addEventListener('click', e => {
+
+  navbar.classList.toggle('sidebar-open');
 
 })
