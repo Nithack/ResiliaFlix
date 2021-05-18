2f804ead5bc44eac470c8d954315619d3520373b
Caio <caio_f.alves@hotmail.com>
2021-05-18 19:48:59 -0300

    Otimização Contato

--- js/contato.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector ('.navbar-toggler');
-const navbar = document.querySelector ('.navbar');
-
-btn.addEventListener('click', e => {
-
-  navbar.classList.toggle('sidebar-open');
-
-})
